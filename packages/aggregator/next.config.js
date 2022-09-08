216c377f768ba98b0149ad0fffd3f9880566ce4e
pg-dev <tomasz.tabiaszewski@wp.pl>
2022-09-09 00:15:14 +0200

    feat: change next configs

--- packages/aggregator/next.config.js
+++ packages/aggregator/next.config.js
@@ -1,7 +1,33 @@
+const { FRANCE_URL, BELGIUM_URL } = process.env;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    async rewrites() {
+      return [
+          {
+              source: '/:path*',
+              destination: '/:path*',
+          },
+          {
+              source: '/fr',
+              destination: `${FRANCE_URL}/fr`
+          },
+          {
+              source: '/fr/:path*',
+              destination: `${FRANCE_URL}/fr/:path*`
+          },
+          {
+              source: '/be',
+              destination: `${BELGIUM_URL}/be`
+          },
+          {
+              source: '/be/:path*',
+              destination: `${BELGIUM_URL}/be/:path*`
+          },
+      ];
+    }
 }
 
 module.exports = nextConfig
