5b93d7eb715604df3be67b8e7a031a9da6d8789f
pg-dev <tomasz.tabiaszewski@wp.pl>
2022-09-09 09:41:37 +0200

    chore: changes in next.config.js

--- packages/aggregator/next.config.js
+++ packages/aggregator/next.config.js
@@ -5,29 +5,29 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     async rewrites() {
-      return [
-          {
-              source: '/:path*',
-              destination: '/:path*',
-          },
-          {
-              source: '/fr',
-              destination: `${FRANCE_URL}/fr`
-          },
-          {
-              source: '/fr/:path*',
-              destination: `${FRANCE_URL}/fr/:path*`
-          },
-          {
-              source: '/be',
-              destination: `${BELGIUM_URL}/be`
-          },
-          {
-              source: '/be/:path*',
-              destination: `${BELGIUM_URL}/be/:path*`
-          },
-      ];
-    }
+        return [
+            {
+                source: "/:path*",
+                destination: `/:path*`,
+            },
+            {
+                source: "/fr",
+                destination: `${FRANCE_URL}/fr`,
+            },
+            {
+                source: "/fr/:path*",
+                destination: `${FRANCE_URL}/fr/:path*`,
+            },
+            {
+                source: "/be",
+                destination: `${BELGIUM_URL}/be`,
+            },
+            {
+                source: "/be/:path*",
+                destination: `${BELGIUM_URL}/be/:path*`,
+            },
+        ];
+    },
 }
 
 module.exports = nextConfig
